74f4a37c335573e715ed41497e86e2756a925cb1
Song Yang <songyang1218@gmail.com>
2019-11-30 12:25:02 -0500

    Fix mesh problem

--- src/comp557/a4/Mesh.java
+++ src/comp557/a4/Mesh.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
 public class Mesh extends Intersectable {
@@ -20,135 +21,106 @@ public class Mesh extends Intersectable {
 	 * The polygon soup.
 	 */
 	public PolygonSoup soup;
+	
+	/** The max depth of a bounding volume tree, 2 by default
+	 * Depth = 1 means just a box around the mesh */
+	private int maxHierarchy = 2;
 
 	public Mesh() {
+		
 		super();
 		this.soup = null;
 	}			
-	
-	/** Cached normals of every face, computed once at the beginning of intersections */
-	public List<Vector3d> normals = new ArrayList<Vector3d>();
-	
-	/** Cached edges of every faces, computed once at the beginning of intersections
-	 * edges[i]= {edgeAB, edgeBC, edgeCA} */
-	public List<Vector3d[]> edges = new ArrayList<Vector3d[]>();
-	
-	private boolean prepared = false;
-	
-	public void prepare() {
-		// assume triangles!
-		List<PolygonSoup.Vertex> list = this.soup.vertexList;
 
-		for (int[] face : this.soup.faceList) {
-			Vector3d normal = new Vector3d();
-			Vector3d edgeAB = new Vector3d();
-			Vector3d edgeBC = new Vector3d();
-			Vector3d edgeCA = new Vector3d();
-			
-			edgeAB.sub(list.get(face[1]).p, list.get(face[0]).p);
-			edgeBC.sub(list.get(face[2]).p, list.get(face[1]).p);
-			edgeCA.sub(list.get(face[0]).p, list.get(face[2]).p);
-			
-			Vector3d[] triangle = new Vector3d[3];
-			triangle[0] = edgeAB;
-			triangle[1] = edgeBC;
-			triangle[2] = edgeCA;
-
-			this.edges.add(triangle);
-
-			normal.cross(edgeAB, edgeBC);
-			normal.normalize();
-
-			this.normals.add(normal);
-		}
-	}
-	
+	/**
+	 * Uses barycentric coordinates, as opposed to the slides
+	 */
 	@Override
 	public void intersect(Ray ray, IntersectResult result) {
 		
-		// TODO: Objective 9: ray triangle intersection for meshes
-		
-		// To avoid calculating the same thing over and over, cache the face normals
-		if (!prepared) {
-			prepared = true;
-			
-			this.prepare();
-		}
-		
-		// For every face, look for the intersection and perform inside-edge test
-		Iterator<Vector3d[]> edgesIterator = this.edges.iterator();
-		Iterator<Vector3d> normalsIterator = this.normals.iterator();
-		Iterator<int[]> faceIterator = this.soup.faceList.iterator();
-	
-		
-		while(edgesIterator.hasNext() && normalsIterator.hasNext() && faceIterator.hasNext()) {
-			Vector3d[] triangle = (Vector3d[]) edgesIterator.next();
-			Vector3d normal = (Vector3d) normalsIterator.next();
-			int[] face = (int[]) faceIterator.next();
-			
-			// there is an intersection
-			if (intersectRayTriangle(ray, face, triangle, normal, result)) {
-				// intersection's t value is in result now
-				result.material = this.material;
-				result.n.set(normal);
-				ray.getPoint(result.t, result.p);
-				
-				return;
-			}
-			
-		}
-		
-		// If no face is intersected, no intersection
-		result.t = Double.POSITIVE_INFINITY;
-	}
-	
-	/** 
-	 * According to the slide 21-23 in ray tracing
-	 * @param ray A ray
-	 * @param face An array of vertex indices belonging to the face
-	 * @param triangle An array of 2 edges of the triangle
-	 * @param result Where the intersection is put, if any
-	 * @return True if an intersection is found
-	 */
-	private boolean intersectRayTriangle(Ray ray, int[] face, Vector3d[] triangle,
-			Vector3d normal, IntersectResult result) {
-		// temporary variables
-		Vector3d temp = new Vector3d();
-		Vector3d aToX = new Vector3d();
-		Vector3d bToX = new Vector3d();
-		Vector3d cToX = new Vector3d();
-		
-		// get the t that intersects the plane
-		double nom, denom;
-		List<PolygonSoup.Vertex> vertices = this.soup.vertexList;
-		
-		denom = ray.viewDirection.dot(normal); // d dot n
-		if (denom < EPSILON && denom > -EPSILON) return false; // ray parallel to the plane
-		
-		temp.sub(vertices.get(face[0]).p, ray.eyePoint); // a - p
-		nom = temp.dot(normal); // (a - p) dot n
-		
-		result.t = nom / denom;
-		
-		if (result.t < EPSILON) return false; // wrong ray direction
-		
-		ray.getPoint(result.t, result.p); // compute intersection with plane
-		
-		aToX.sub(result.p, vertices.get(face[0]).p); // x - a
-		temp.cross(triangle[0], aToX); // (b - a) x (x - a)
-		if (temp.dot(normal) < -EPSILON) return false;
-		
-		bToX.sub(result.p, vertices.get(face[1]).p); // x - b
-		temp.cross(triangle[1], bToX); // (c - b) x (x - b)
-		if (temp.dot(normal) < -EPSILON) return false;
-		
-		cToX.sub(result.p, vertices.get(face[2]).p); // x - c
-		temp.cross(triangle[2], cToX); // (a - c) x (x - c)
-		if (temp.dot(normal) < -EPSILON) return false;
-		
-		// intersection is in the triangle
-		
-		return true;
-	}
+		// TODO: Objective 7: ray triangle intersection for meshes
+		// only triangles!!
+		// using barycentric coordinates
 
+		Vector3d v0v1 = new Vector3d();
+		Vector3d v0v2 = new Vector3d();
+		Vector3d pvec = new Vector3d();
+		
+		double det, invdet, u, v, t;
+		
+		Vector3d tvec = new Vector3d();
+		Vector3d qvec = new Vector3d();
+		
+		result.p = new Point3d();
+		result.n = new Vector3d();
+		
+		// Set the current result to nothing, so this would be
+		// the "return" value of t if nothing is found
+		result.t = Double.POSITIVE_INFINITY;
+		
+		for (int[] face : soup.faceList) {
+
+			//3 points of triangle
+			Point3d p0 = soup.vertexList.get(face[0]).p;
+			Point3d p1 = soup.vertexList.get(face[1]).p;
+			Point3d p2 = soup.vertexList.get(face[2]).p;
+
+			
+			v0v1.sub(p1, p0); // one side of the triangle
+			
+			v0v2.sub(p2, p0); // second side of the triangle
+
+			pvec.cross(ray.viewDirection,v0v2);
+			det = v0v1.dot(pvec);
+
+			if(Math.abs(det)< EPSILON) continue; // parallel to the triangle
+
+			invdet = 1/det;
+			
+			tvec.sub(ray.eyePoint,p0);
+			u = tvec.dot(pvec)*invdet;
+			if(u<0||u>1) continue;
+			
+			
+			qvec.cross(tvec, v0v1);
+			v = ray.viewDirection.dot(qvec)*invdet;
+			if(v<0||u+v>1) continue;
+			
+			t = v0v2.dot(qvec)*invdet;
+			
+			if (t>EPSILON && t<result.t) {
+				result.t = t;
+				
+				ray.getPoint(result.t, result.p);
+				result.material = material;
+				
+				result.n.cross(v0v1, v0v2);
+				result.n.normalize();
+			}
+		}
+		
+	}
+	
+	/**
+	 * Prepares the mesh for computation. Sets the max_hierarchy
+	 * and many vectors.
+	 */
+	public void prepare() {
+		// This method calculates how many divisions of a 
+		// bounding volume are needed
+		// Rule: the smallest volume contains <1000 vertices
+		
+		// Let n be the number of vertices
+		// Then, n / #volumes = 1000
+		// # volumes = 1000 / n
+		// # volumes = 8^depth - 1 
+	}
+	
+	/**
+	 * Builds a hierarchical bounding volume of the mesh
+	 * Recursive function
+	 */
+	public void buildBoundingVolumes(int depth) {
+		
+	}
 }
